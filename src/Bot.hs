7de7b58931542c7f1577c8a09f779a3c311b6a87
rexim <reximkut@gmail.com>
2018-02-07 18:16:20 +0700

    (#35) refactor Bot.effectOfCommand

--- src/Bot.hs
+++ src/Bot.hs
@@ -5,9 +5,10 @@ import Command
 import qualified Data.Map as M
 import Data.Maybe
 import qualified Data.Text as T
+import Data.Time
 import Effect
-import Russify
 import Entity
+import Russify
 
 data Event = Join
            | Msg T.Text T.Text
@@ -19,31 +20,37 @@ bot Join = say $ T.pack "HyperNyard"
 bot (Msg user text) = maybe (return ()) (effectOfCommand user) $ textAsCommand text
 
 effectOfCommand :: T.Text -> Command T.Text -> Effect ()
-effectOfCommand sender command =
-    -- TODO(#35): refactor out case in effectOfCommand
-    -- Use function level pattern matching
-    case T.unpack $ commandName command of
-      "russify" -> replyToUser sender
-                   $ russify
-                   $ commandArgs command
-      "addquote" -> do timestamp <- now
-                       let entity =
-                               Entity { entityName = "quote"
-                                      , entityProperties =
-                                          M.fromList [ ("content", PropertyText $ commandArgs command)
-                                                     , ("quoter", PropertyText $ sender)
-                                                     , ("timestamp", PropertyUTCTime $ timestamp)
-                                                     ]
-                                      }
-                       entityId <- saveEntity entity
-                       replyToUser sender $ T.concat [ "Added the quote under the number "
-                                                     , T.pack $ show entityId
-                                                     ]
-      -- TODO(#36): implement !quote command
-      _ -> return ()
+effectOfCommand sender (Command { commandName = "russify"
+                                , commandArgs = westernSpyMsg }) =
+    replyToUser sender $ russify westernSpyMsg
+effectOfCommand sender (Command { commandName = "addquote"
+                                , commandArgs = quoteContent }) =
+    (quoteEntity quoteContent sender <$> now)
+    >>= saveEntity
+    >>= quoteAddedReply sender
+
+-- TODO(#36): implement !quote command
+
+effectOfCommand _ _ = return ()
 
 replyToUser :: T.Text -> T.Text -> Effect ()
 replyToUser user text = say $ T.concat [ (T.pack "@")
                                        , user
                                        , (T.pack " ")
                                        , text]
+
+quoteAddedReply :: T.Text -> Int -> Effect ()
+quoteAddedReply user quoteId =
+    replyToUser user $ T.concat [ "Added the quote under the number "
+                                , T.pack $ show quoteId
+                                ]
+
+quoteEntity :: T.Text -> T.Text -> UTCTime -> Entity
+quoteEntity content quoter timestamp =
+    Entity { entityName = "quote"
+           , entityProperties =
+               M.fromList [ ("content", PropertyText content)
+                          , ("quoter", PropertyText quoter)
+                          , ("timestamp", PropertyUTCTime timestamp)
+                          ]
+           }
