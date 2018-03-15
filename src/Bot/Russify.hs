0560fc74518dfb39505fe5fd42a9230f78a3d16d
rexim <reximkut@gmail.com>
2018-03-15 17:05:24 +0700

    Merge Bot.Russify and Russify modules

--- src/Bot/Russify.hs
+++ src/Bot/Russify.hs
@@ -1,9 +1,15 @@
-module Bot.Russify where
+{-# LANGUAGE TemplateHaskell #-}
+module Bot.Russify (russifyCommand) where
 
 import           Bot.Replies
+import           Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as B
+import           Data.FileEmbed
+import qualified Data.Map.Lazy as M
+import           Data.Maybe
 import qualified Data.Text as T
+import           Data.Text.Encoding
 import           Effect
-import           Russify
 import           Text.Printf
 
 russifyCommand :: T.Text -> T.Text -> Effect ()
@@ -12,3 +18,18 @@ russifyCommand sender westernSpyMsg =
     $ T.pack
     $ printf "%s KKomrade"
     $ russify westernSpyMsg
+
+mazarusha :: M.Map T.Text T.Text
+mazarusha =
+    fromMaybe M.empty
+    $ decode
+    $ B.fromStrict
+    $ encodeUtf8
+    $ $(embedStringFile "./resources/mazarusha.json")
+
+russifyChar :: Char -> T.Text
+russifyChar x = fromMaybe (T.pack [x]) $ M.lookup (T.pack [x]) mazarusha
+
+russify :: T.Text -> T.Text
+russify text =
+    T.concatMap russifyChar text
