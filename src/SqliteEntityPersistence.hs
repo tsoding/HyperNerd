48bfe114fdf37c5fe1dad2e4b9e31df36a9dc7a4
rexim <reximkut@gmail.com>
2018-06-04 12:59:48 +0700

    TODO(#134)

--- src/SqliteEntityPersistence.hs
+++ src/SqliteEntityPersistence.hs
@@ -160,6 +160,6 @@ selectEntities :: Connection -> T.Text -> Selector -> IO [Entity]
 selectEntities conn name All =
     do ids <- getAllEntityIds conn name
        fromMaybe [] . traverse id <$> traverse (getEntityById conn name) ids
--- TODO: PropertyEquals selector is not handled in SEP.selectEntities
+-- TODO(#134): PropertyEquals selector is not handled in SEP.selectEntities
 selectEntities conn name (PropertyEquals _ _) =
     selectEntities conn name All
