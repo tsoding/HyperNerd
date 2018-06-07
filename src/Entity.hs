0563e35102fadc4a98f049a5990b5eb0df8872cb
rexim <reximkut@gmail.com>
2018-06-07 23:33:57 +0700

    (#132) Remove propertyAs* function

--- src/Entity.hs
+++ src/Entity.hs
@@ -35,49 +35,31 @@ extractProperty fieldName entity =
                                                                     (entityId entity)))
                    return
                    (M.lookup fieldName $ entityProperties entity)
-       either (throwM . EntityException . T.pack . printf "Cannot parse field '%s' of entity '%s' with id %d: %s"
-                                                          fieldName
-                                                          (entityName entity)
-                                                          (entityId entity))
-              return
-              (fromProperty property)
+       fromProperty property
 
 class IsEntity e where
     toProperties :: e -> Properties
     fromEntity :: MonadThrow m => Entity -> m e
 
 class IsProperty a where
-    fromProperty :: Property -> Either T.Text a
+    fromProperty :: MonadThrow m => Property -> m a
 
 instance IsProperty Int where
-    fromProperty (PropertyInt x) = Right x
-    fromProperty _ = Left "Expected type Int"
+    fromProperty (PropertyInt x) = return  x
+    fromProperty _ = throwM $ EntityException "Could parse a field. Expected type Int"
 
 instance IsProperty T.Text where
-    fromProperty (PropertyText x) = Right x
-    fromProperty _ = Left "Expected type Text"
+    fromProperty (PropertyText x) = return x
+    fromProperty _ = throwM $ EntityException "Could parse a field. Expected type Text"
 
 instance IsProperty UTCTime where
-    fromProperty (PropertyUTCTime x) = Right x
-    fromProperty _ = Left "Expected type UTCTime"
+    fromProperty (PropertyUTCTime x) = return x
+    fromProperty _ = throwM $ EntityException "Could parse a field. Expected type UTCTime"
 
 propertyTypeName :: Property -> String
 propertyTypeName (PropertyInt _) = "PropertyInt"
 propertyTypeName (PropertyText _) = "PropertyText"
 propertyTypeName (PropertyUTCTime _) = "PropertyUTCTime"
-
--- TODO(#132): propertyAs* functions duplicates IsProperty typeclass
-propertyAsInt :: Property -> Maybe Int
-propertyAsInt (PropertyInt x) = Just x
-propertyAsInt _ = Nothing
-
-propertyAsText :: Property -> Maybe T.Text
-propertyAsText (PropertyText x) = Just x
-propertyAsText _ = Nothing
-
-propertyAsUTCTime :: Property -> Maybe UTCTime
-propertyAsUTCTime (PropertyUTCTime x) = Just x
-propertyAsUTCTime _ = Nothing
 
 restoreProperty :: (T.Text, T.Text, Maybe Int, Maybe T.Text, Maybe UTCTime) -> Maybe (T.Text, Property)
 restoreProperty (name, "PropertyInt", Just x,      _, _) = Just (name, PropertyInt x)
