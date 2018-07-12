6abc677ebee8a553c7649a18d130d8b00cdc9cb6
rexim <reximkut@gmail.com>
2018-07-12 23:39:59 +0700

    (#165) Introduce Bot.Periodic module

---
+++ src/Bot/Periodic.hs
@@ -0,0 +1,12 @@
+module Bot.Periodic ( addPeriodicMessage
+                    , startPeriodicMessages
+                    ) where
+
+import qualified Data.Text as T
+import           Effect
+
+addPeriodicMessage :: T.Text -> Effect ()
+addPeriodicMessage = undefined
+
+startPeriodicMessages :: Effect ()
+startPeriodicMessages = undefined
