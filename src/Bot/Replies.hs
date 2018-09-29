e174a7beb6bfa9b5f8fb3997fa964840d6b4a13c
rexim <reximkut@gmail.com>
2018-09-30 06:51:36 +0700

    Remove printf from Bot.Replies module

--- src/Bot/Replies.hs
+++ src/Bot/Replies.hs
@@ -3,21 +3,20 @@ module Bot.Replies where
 
 import qualified Data.Text as T
 import           Effect
-import           Text.Printf
 import           Events
 import           Text.InterpolatedString.QM
 
 replyToUser :: T.Text -> T.Text -> Effect ()
-replyToUser user text = say $ T.pack $ printf "@%s %s" user text
+replyToUser user text = say [qms|@{user} {text}|]
 
 replyToSender :: Sender -> T.Text -> Effect ()
 replyToSender sender = replyToUser (senderName sender)
 
 banUser :: T.Text -> Effect ()
-banUser user = say [qm|/ban {user}|]
+banUser user = say [qms|/ban {user}|]
 
 timeoutUser :: Int -> T.Text -> Effect ()
-timeoutUser t user = say [qm|/timeout {user} {t}|]
+timeoutUser t user = say [qms|/timeout {user} {t}|]
 
 timeoutSender :: Int -> Sender -> Effect ()
 timeoutSender t sender = timeoutUser t (senderName sender)
