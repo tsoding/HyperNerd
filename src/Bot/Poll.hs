b29e8acebe2df095275d553589fa13090748edd0
rexim <reximkut@gmail.com>
2018-10-01 21:54:42 +0700

    (#89, #88) announcePollResults, registerVote
    Close #89
    Close #88

--- src/Bot/Poll.hs
+++ src/Bot/Poll.hs
@@ -15,6 +15,7 @@ import           Events
 import           Property
 import           Text.InterpolatedString.QM
 import           Command
+import           Data.Function
 
 data PollOption = PollOption { poPollId :: Int
                              , poName :: T.Text
@@ -22,6 +23,10 @@ data PollOption = PollOption { poPollId :: Int
 
 data Poll = Poll { pollAuthor :: T.Text
                  , pollStartedAt :: UTCTime
+                 }
+
+data Vote = Vote { voteUser :: T.Text
+                 , voteOptionId :: Int
                  }
 
 instance IsEntity Poll where
@@ -40,6 +45,14 @@ instance IsEntity PollOption where
         PollOption <$> extractProperty "pollId" properties
                    <*> extractProperty "name" properties
 
+instance IsEntity Vote where
+    toProperties vote = M.fromList [ ("user", PropertyText $ voteUser vote)
+                                   , ("optionId", PropertyInt $ voteOptionId vote)
+                                   ]
+    fromProperties properties =
+        Vote <$> extractProperty "user" properties
+             <*> extractProperty "optionId" properties
+
 pollCommand :: Sender -> [T.Text] -> Effect ()
 pollCommand sender options =
     do poll <- currentPoll
@@ -49,13 +62,13 @@ pollCommand sender options =
                        optionsList <- return $ T.concat $ intersperse ", " options
                        say [qms|The poll has been started.
                                 Vote for one of the options: {optionsList}|]
-                       timeout 10000 $ announcePollResults pollId
+                       timeout 10500 $ announcePollResults pollId
 
 voteCommand :: Sender -> T.Text -> Effect ()
 voteCommand sender option = do
   poll <- currentPoll
   case poll of
-    Just poll' -> registerVote poll' (senderName sender) option
+    Just poll' -> registerVote poll' sender option
     Nothing    -> replyToSender sender "No polls are in place"
 
 pollLifetime :: UTCTime -> Entity Poll -> Double
@@ -101,10 +114,38 @@ startPoll author options =
                                                 }
        return pollId
 
--- TODO(#88): announcePollResults is not implemented
 announcePollResults :: Int -> Effect ()
-announcePollResults _ = return ()
+announcePollResults pollId = do
+  optionIds <- fmap (map entityId) $
+               (selectEntities "PollOption" $
+                Filter (PropertyEquals "pollId" $
+                        PropertyInt pollId) All :: Effect [Entity PollOption])
+  votes <- mapM (\optionId -> selectEntities "Vote" $
+                              Filter (PropertyEquals "optionId" $
+                                      PropertyInt optionId) All) optionIds
+  let results = sortBy (flip compare `on` length) votes
+  case results of
+    ((winnerVote:_):_) -> do winnerOption <- getEntityById "PollOption" (voteOptionId $
+                                                                         entityPayload $
+                                                                         winnerVote)
+                             case winnerOption of
+                               Just winnerOption' -> say [qms|The poll has finished! The winner is
+                                                              {poName $ entityPayload $ winnerOption'}|]
+                               Nothing            -> logMsg [qms|[WARNING] could not retrieve
+                                                                 winner option|]
+    _                  -> say [qms|Poll has finished but nobody voted
+                                   PepeHands Dead stream PepeHands|]
 
--- TODO(#89): voteCommand is not implemented
-registerVote :: Entity Poll -> T.Text -> T.Text -> Effect ()
-registerVote _ _ _ = return ()
+registerVote :: Entity Poll -> Sender -> T.Text -> Effect ()
+registerVote poll sender optionName = do
+  options <- selectEntities "PollOption" $
+             Filter (PropertyEquals "pollId" $
+                     PropertyInt $
+                     entityId poll) All
+  case find ((== optionName) . poName . entityPayload) options of
+    Just option -> do _ <- createEntity "Vote" Vote { voteUser = senderName sender
+                                                    , voteOptionId = entityId option
+                                                    }
+                      return ()
+    Nothing -> logMsg [qms|[WARNING] {senderName sender} voted for
+                           unexisting option {optionName}|]
