9076ff5b1cb4fe1e322cbb74aa6e99a916caffd2
rexim <reximkut@gmail.com>
2018-10-25 14:01:25 +0700

    Introduce core Reaction vocabulary

--- src/Reaction.hs
+++ src/Reaction.hs
@@ -2,7 +2,17 @@ module Reaction where
 
 import Effect
 import Events
+import Control.Monad
 
 newtype Reaction a = Reaction { runReaction :: a -> Effect () }
 
 type MsgReaction a = Reaction (Message a)
+
+cmap :: (a -> b) -> Reaction b -> Reaction a
+cmap f reaction = Reaction (runReaction reaction . f)
+
+cmapF :: Functor f => (a -> b) -> Reaction (f b) -> Reaction (f a)
+cmapF f reaction = Reaction (runReaction reaction . fmap f)
+
+liftE :: (a -> Effect b) -> Reaction b -> Reaction a
+liftE f reaction = Reaction $ (f >=> runReaction reaction)
