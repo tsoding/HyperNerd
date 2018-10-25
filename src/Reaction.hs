7ed9c807da20c20baf02a0aa82649ef557d1195c
rexim <reximkut@gmail.com>
2018-10-25 13:52:47 +0700

    Remove dependency on contravariant

--- src/Reaction.hs
+++ src/Reaction.hs
@@ -1,19 +1,8 @@
 module Reaction where
 
-import Data.Functor.Contravariant
-import Data.Functor.Contravariant.Compose
 import Effect
 import Events
 
 newtype Reaction a = Reaction { runReaction :: a -> Effect () }
 
-instance Contravariant Reaction where
-    contramap f reaction = Reaction $ runReaction reaction . f
-
-type MsgReaction a = ComposeCF Reaction Message a
-
-msgReaction :: (Message a -> Effect ()) -> MsgReaction a
-msgReaction = ComposeCF . Reaction
-
-runMsgReaction :: MsgReaction a -> Message a -> Effect ()
-runMsgReaction = runReaction . getComposeCF
+type MsgReaction a = Reaction (Message a)
