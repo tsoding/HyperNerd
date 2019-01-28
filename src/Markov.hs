23386a3c679035e8041892ad1bab293e41f35f76
rexim <reximkut@gmail.com>
2019-01-28 14:33:12 +0700

    Double Markov events

--- src/Markov.hs
+++ src/Markov.hs
@@ -10,8 +10,10 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 import Data.Text.Encoding
 import qualified Data.Text.IO as TIO
-import Safe
 import System.Random
+import Control.Monad
+import Data.Maybe
+import Safe
 
 data Event
   = Begin
@@ -36,12 +38,12 @@ instance ToField Event where
   toField (Word word) = BS.append eventWordPrefix $ encodeUtf8 word
 
 newtype Markov = Markov
-  { asMap :: M.Map Event (M.Map Event Int)
+  { asMap :: M.Map (Event, Event) (M.Map Event Int)
   } deriving (Show, Read)
 
 instance FromRecord Markov where
   parseRecord v
-    | length v == 3 = record2Markov <$> v .! 0 <*> v .! 1 <*> v .! 2
+    | length v == 4 = record2Markov <$> v .! 0 <*> v .! 1 <*> v .! 2 <*> v .! 3
     | otherwise = mempty
 
 emptyMarkov :: Markov
@@ -57,16 +59,21 @@ instance Semigroup Markov where
 instance Monoid Markov where
   mempty = emptyMarkov
 
-record2Markov :: Event -> Event -> Int -> Markov
-record2Markov cause result occurance =
-  Markov $ M.fromList [(cause, M.fromList [(result, occurance)])]
+record2Markov :: Event -> Event -> Event -> Int -> Markov
+record2Markov cause1 cause2 result occurance =
+  Markov $ M.fromList [((cause1, cause2), M.fromList [(result, occurance)])]
 
-singleton :: (Event, Event) -> Markov
-singleton (e1, e2) = record2Markov e1 e2 1
+singleton :: (Event, Event, Event) -> Markov
+singleton (e1, e2, e3) = record2Markov e1 e2 e3 1
+
+scan3 :: [a] -> [(a, a, a)]
+scan3 xs =
+  join $
+  maybeToList $ liftM3 zip3 (return xs) (tailMay xs) (tailMay xs >>= tailMay)
 
 text2Markov :: T.Text -> Markov
 text2Markov text =
-  maybe mempty (foldMap singleton . zip events) (tailMay events)
+  foldMap singleton $ scan3 events
   where
     events = [Begin] ++ map Word (T.words text) ++ [End]
 
@@ -76,7 +83,7 @@ log2Markov = foldMap text2Markov . filter (not . T.isPrefixOf "!")
 file2Markov :: FilePath -> IO Markov
 file2Markov filePath = log2Markov . T.lines <$> TIO.readFile filePath
 
-nextEvent :: Markov -> Event -> IO Event
+nextEvent :: Markov -> (Event, Event) -> IO Event
 nextEvent markov event =
   case M.lookup event (asMap markov) of
     Just stat -> do
@@ -97,20 +104,30 @@ eventsAsText (Begin:rest) = eventsAsText rest
 eventsAsText (End:rest) = eventsAsText rest
 eventsAsText (Word word:rest) = T.concat [word, " ", eventsAsText rest]
 
-simulateFrom :: Event -> Markov -> IO [Event]
-simulateFrom End _ = return []
-simulateFrom event markov = do
-  event' <- nextEvent markov event
-  (event' :) <$> simulateFrom event' markov
+simulateFrom :: Markov -> (Event, Event) -> IO [Event]
+simulateFrom _ (event, End) = return [event]
+simulateFrom markov (event1, event2) = do
+  event3 <- nextEvent markov (event1, event2)
+  (event1 :) <$> simulateFrom markov (event2, event3)
+
+randomElement :: [a] -> IO a
+randomElement xs = do
+  let n = length xs
+  i <- randomRIO (0, n - 1)
+  return (xs !! i)
+
+randomStartEvent :: Markov -> IO (Event, Event)
+randomStartEvent markov =
+  randomElement $ filter ((==) Begin . fst) $ M.keys $ asMap markov
 
 simulate :: Markov -> IO [Event]
-simulate = simulateFrom Begin
+simulate markov = randomStartEvent markov >>= simulateFrom markov
 
-markov2Records :: Markov -> [(Event, Event, Int)]
+markov2Records :: Markov -> [(Event, Event, Event, Int)]
 markov2Records markov = do
-  (cause, results) <- M.toList $ asMap markov
+  ((cause1, cause2), results) <- M.toList $ asMap markov
   (result, occurance) <- M.toList results
-  return (cause, result, occurance)
+  return (cause1, cause2, result, occurance)
 
 saveMarkov :: FilePath -> Markov -> IO ()
 saveMarkov filePath markov =
