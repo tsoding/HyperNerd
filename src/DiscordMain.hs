9eb60adebb27d6f69eec8d1fa7cdebf4147a9eb2
rexim <reximkut@gmail.com>
2019-02-08 17:02:59 +0700

    Add TODO(#448)

--- src/DiscordMain.hs
+++ src/DiscordMain.hs
@@ -3,7 +3,7 @@ module Main where
 -- TODO(#447): We need a simple proof-of-concept of a Discord Bot in Haskell
 --   Should be implemented in src/DiscordMain.hs which is an entry
 --   point of Discord executable.
--- TODO: How can we integrate Discord Bot proof-of-concept with HyperNerd?
+-- TODO(#448): How can we integrate Discord Bot proof-of-concept with HyperNerd?
 
 main :: IO ()
 main = undefined
