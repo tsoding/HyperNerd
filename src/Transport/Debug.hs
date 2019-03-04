6618b55ab4cfcbde5e62a2a9eb97fc7eb297a78a
rexim <reximkut@gmail.com>
2019-03-05 02:36:56 +0700

    Implement debugRepl

--- src/Transport/Debug.hs
+++ src/Transport/Debug.hs
@@ -3,10 +3,34 @@ module Transport.Debug (debugTransportEntry) where
 import Transport
 import Config
 import Control.Concurrent.STM
+import qualified Data.Text as T
 
--- TODO: debugRepl is not implemented
+-- TODO: flushOutcomingMessages is not implemented
+flushOutcomingMessages :: OutcomingQueue -> IO ()
+flushOutcomingMessages = undefined
+
 debugRepl :: IncomingQueue -> OutcomingQueue -> DebugParams -> IO ()
-debugRepl = undefined
+debugRepl incoming outcoming config = do
+  flushOutcomingMessages outcoming
+  putStr "> "
+  message <- getLine
+  atomically $
+    writeTQueue incoming $
+    InMsg
+      Sender
+        { senderName = dbgOwner config
+        , senderDisplayName = dbgOwner config
+        , senderChannel = DebugChannel
+        -- TODO: Sender id and roles are not configurable in Debug Mode
+        --   We can make them configurable via the config file
+        --   and/or via CLI commands.
+        , senderId = ""
+        , senderSubscriber = True
+        , senderMod = True
+        , senderBroadcaster = True
+        , senderOwner = True
+        }
+      (T.pack message)
 
 debugTransportEntry :: IncomingQueue -> OutcomingQueue -> DebugParams -> IO ()
 debugTransportEntry incoming outcoming config = do
