354e455ce1672d035eb31642bb5d697e2020b79e
rexim <reximkut@gmail.com>
2019-03-05 03:34:54 +0700

    Add TODO(#479)

--- src/BotState.hs
+++ src/BotState.hs
@@ -150,7 +150,7 @@ applyEffect (botState, Free (GetTransport s)) =
   case bsConfig botState of
     TwitchConfig _ -> return (botState, s TwitchTransport)
     DiscordConfig _ -> return (botState, s DiscordTransport)
-    -- TODO: Result of getTransport Effect is hardcoded in Debug Mode
+    -- TODO(#479): Result of getTransport Effect is hardcoded in Debug Mode
     --    I think it should be configurable via config file
     --    or/and CLI commands of Debug Mode
     DebugConfig _ -> return (botState, s TwitchTransport)
