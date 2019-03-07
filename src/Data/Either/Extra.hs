94a8346168d061206dea92e62ab35a8495616bf4
rexim <reximkut@gmail.com>
2019-03-07 23:57:17 +0700

    (#476) Implement mapLeft

--- src/Data/Either/Extra.hs
+++ src/Data/Either/Extra.hs
@@ -3,3 +3,7 @@ module Data.Either.Extra where
 maybeToEither :: a -> Maybe b -> Either a b
 maybeToEither left Nothing = Left left
 maybeToEither _ (Just right) = Right right
+
+mapLeft :: (a -> c) -> Either a b -> Either c b
+mapLeft f (Left x) = Left $ f x
+mapLeft _ (Right x) = Right x
