433a45048f7f80d6cf4aa4b91aaacea8c9d50c53
rexim <reximkut@gmail.com>
2019-04-12 21:19:28 +0700

    Revert "Fix hindent remarks"
    This reverts commit c860f4add834ddec6c9f45e09e280c51964d4cd8.

--- src/Bot.hs
+++ src/Bot.hs
@@ -35,7 +35,6 @@ import Data.Foldable
 import Data.Functor.Compose
 import Data.Functor.Identity
 import qualified Data.Map as M
-import Data.Maybe
 import Data.Proxy
 import qualified Data.Text as T
 import Effect
@@ -50,6 +49,7 @@ import qualified Text.Regex.Base.RegexLike as Regex
 import Text.Regex.TDFA (defaultCompOpt, defaultExecOpt)
 import Text.Regex.TDFA.String
 import Transport
+import Data.Maybe
 
 type Bot = InEvent -> Effect ()
 
