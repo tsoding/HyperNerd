b82e1bdf9992fbc04a960338d43629ba5dda2fc5
rexim <reximkut@gmail.com>
2019-05-10 05:00:25 +0700

    (#603) Add support to detached HEAD for Bot.DocLoc

--- src/Bot/DocLoc.hs
+++ src/Bot/DocLoc.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Bot.DocLoc where
+module Bot.DocLoc
+  ( githubLinkLocationStr
+  , githubLinkLocation
+  ) where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -11,15 +14,18 @@ import Text.InterpolatedString.QM
 githubLinkLocationStr :: Q Exp
 githubLinkLocationStr = LitE . StringL <$> githubLinkLocation
 
+gitHead :: IO String
+gitHead = do
+  headContent <- T.strip <$> TIO.readFile ".git/HEAD"
+  if T.take 5 headContent == "ref: "
+    then T.unpack . T.strip <$>
+         TIO.readFile (T.unpack $ T.append ".git/" $ T.drop 5 $ headContent)
+    else return $ T.unpack headContent
+
 githubLinkLocation :: Q String
 githubLinkLocation = do
   filename <- loc_filename <$> location
   line <- fst . loc_start <$> location
-  rev <-
-    runIO $ do
-      refFilename <-
-        T.unpack . T.append ".git/" . T.drop 5 . T.strip <$>
-        TIO.readFile ".git/HEAD"
-      T.unpack . T.strip <$> TIO.readFile refFilename
+  rev <- runIO gitHead
   return
     [qms|https://github.com/tsoding/HyperNerd/blob/{rev}/{filename}#L{line}|]
