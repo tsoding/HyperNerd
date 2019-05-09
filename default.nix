d167075f8f5913e01f22162fdba484c91f42dc9f
rexim <reximkut@gmail.com>
2019-05-09 23:10:05 +0700

    (#604) add ghcid to the NixOS development environment

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {}; {
     HyperNerdEnv = stdenv.mkDerivation {
         name = "HyperNerdEnv";
-        buildInputs = [ ghc cabal-install openssl zlib ];
+        buildInputs = [ ghc cabal-install openssl zlib haskellPackages.ghcid ];
         LD_LIBRARY_PATH="${openssl.out}/lib;${zlib}/lib";
     };
 }
