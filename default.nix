e2c14a9232577523edd923d88cb08fe334d5da4a
rexim <reximkut@gmail.com>
2019-05-09 23:16:21 +0700

    (#604) Add hindent to the NixOS development environment

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
 with import <nixpkgs> {}; {
     HyperNerdEnv = stdenv.mkDerivation {
         name = "HyperNerdEnv";
-        buildInputs = [ ghc cabal-install openssl zlib haskellPackages.ghcid ];
+        buildInputs = [ ghc
+                        cabal-install
+                        openssl
+                        zlib
+                        haskellPackages.ghcid
+                        haskellPackages.hindent
+                      ];
         LD_LIBRARY_PATH="${openssl.out}/lib;${zlib}/lib";
     };
 }
