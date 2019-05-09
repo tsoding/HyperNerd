b2e4ae7c96ca0b1d6eef325cd040f04601d5f577
rexim <reximkut@gmail.com>
2019-05-10 01:25:36 +0700

    (#603) Fix hindent remarks

--- src/Bot.hs
+++ src/Bot.hs
@@ -14,6 +14,7 @@ import Bot.Banwords
 import Bot.BttvFfz
 import Bot.Calc
 import Bot.CustomCommand
+import Bot.DocLoc
 import Bot.Dubtrack
 import Bot.Friday
 import Bot.Help
@@ -56,7 +57,6 @@ import qualified Text.Regex.Base.RegexLike as Regex
 import Text.Regex.TDFA (defaultCompOpt, defaultExecOpt)
 import Text.Regex.TDFA.String
 import Transport
-import Bot.DocLoc
 
 type Bot = InEvent -> Effect ()
 
