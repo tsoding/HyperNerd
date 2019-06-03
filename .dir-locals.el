3259a2dd548a03044c117246da0fee15774d6bcc
rexim <reximkut@gmail.com>
2019-06-03 23:35:29 +0700

    (#624) Use cabal new-repl for Emacs haskell-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((haskell-mode . ((haskell-process-args-cabal-repl . ("--ghc-option=-ferror-spans"
-                                                      "--ghc-option=-Wno-missing-home-modules"
-                                                      "exe:HyperNerd")))))
+((haskell-mode . ((haskell-process-args-cabal-new-repl . ("--ghc-option=-ferror-spans"
+                                                          "--ghc-option=-Wno-missing-home-modules"
+                                                          "exe:HyperNerd")))))
