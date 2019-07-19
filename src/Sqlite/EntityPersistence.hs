f92cfc0e95b8a6863868ff5415ecd5f2e37dde6e
rexim <reximkut@gmail.com>
2019-07-19 21:42:12 +0700

    (#697) Add timer id to all PeriodicCommands

--- src/Sqlite/EntityPersistence.hs
+++ src/Sqlite/EntityPersistence.hs
@@ -149,6 +149,21 @@ entityMigrations =
           AND propertyText NOT LIKE 'TwitchChannel%'
           AND propertyText NOT LIKE 'DiscordChannel%'
           AND propertyText NOT LIKE '#%';|]
+  , [r| INSERT INTO EntityProperty (
+            entityName,
+            entityId,
+            propertyName,
+            propertyType,
+            propertyInt
+       )
+       SELECT entityName,
+              entityId,
+              'timer' AS propertyName,
+              'PropertyInt' AS propertyType,
+              1 AS propertyInt
+       FROM EntityProperty
+       WHERE entityName = 'PeriodicCommand'
+       GROUP BY entityId; |]
   ]
 
 prepareSchema :: Connection -> IO ()
