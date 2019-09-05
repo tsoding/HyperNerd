3cbef09c613079c6a61eb72d0efb1c52a28bcbf7
rexim <reximkut@gmail.com>
2019-09-06 01:46:25 +0700

    Add TODO(#766)

--- src/Bot.hs
+++ src/Bot.hs
@@ -516,7 +516,7 @@ builtinCommands =
           ( "Count amount of forbidden characters in the message"
           , $githubLinkLocationStr
           , countForbiddenCommand))
-    -- TODO: !asciify command does not cache the asciify results
+    -- TODO(#766): !asciify command does not cache the asciify results
     , ( "asciify"
       , mkBuiltinCommand
           ( "Asciify Twitch, BTTV or FFZ emote"
