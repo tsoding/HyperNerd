5b6a822c9221e4d34e6b6b3283d765f044772ad1
rexim <reximkut@gmail.com>
2019-09-15 18:48:32 +0700

    Remove file-embed

--- src/Bot/Russify.hs
+++ src/Bot/Russify.hs
@@ -8,13 +8,9 @@ module Bot.Russify
   ) where
 
 import Bot.Replies
-import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as B
-import Data.FileEmbed
 import qualified Data.Map.Lazy as M
 import Data.Maybe
 import qualified Data.Text as T
-import Data.Text.Encoding
 import Reaction
 import Transport
 
@@ -26,9 +22,32 @@ derussifyCommand = cmapR (transliterate demazarusha) sayMessage
 
 mazarusha :: M.Map T.Text T.Text
 mazarusha =
-  fromMaybe M.empty $
-  decode $
-  B.fromStrict $ encodeUtf8 $(embedStringFile "./resources/mazarusha.json")
+  M.fromList
+    [ ("a", "д")
+    , ("e", "ё")
+    , ("b", "б")
+    , ("h", "н")
+    , ("k", "к")
+    , ("m", "м")
+    , ("n", "и")
+    , ("o", "ф")
+    , ("r", "г")
+    , ("t", "т")
+    , ("u", "ц")
+    , ("x", "ж")
+    , ("w", "ш")
+    , ("A", "Д")
+    , ("G", "Б")
+    , ("E", "Ё")
+    , ("N", "И")
+    , ("O", "Ф")
+    , ("R", "Я")
+    , ("U", "Ц")
+    , ("W", "Ш")
+    , ("X", "Ж")
+    , ("Y", "У")
+    ]
+
 
 demazarusha :: M.Map T.Text T.Text
 demazarusha =
